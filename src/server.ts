c67efedc75ab31bd45f66f798af01190612e8d01
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-09-24 21:54:30 -0300

    feat: add Category

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.route";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+
+app.listen(3333, () => "Server is running!");
